0a3eb0a9141f4f07eab731530e17198f47245aba
ardikabs <ardikabs@gmail.com>
2018-01-23 21:04:32 +0700

    minor changes

--- init_sensor.sh
+++ init_sensor.sh
@@ -21,4 +21,4 @@ mv ~/sensor/ /home/sensor/sensor
 
 usermod -aG docker sensor
 su - sensor
-docker-compose -f /home/sensor/docker-compose.yml up -d
+docker-compose -f /home/sensor/sensor/docker-compose.yml up -d
